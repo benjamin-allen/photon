6c871011903d98376bb6dcdab3203c6a857b25fc
Nullsrc <beallen529@gmail.com>
2017-07-16 23:04:06 -0500

    Component and ComponentRegistry

---
+++ photon/include/component.hpp
@@ -0,0 +1,60 @@
+#include <string>
+#include <vector>
+
+#pragma once
+
+class Component {
+private:
+	std::string _name;
+	bool _isActive = false;
+
+public:
+	Component(std::string name);
+	std::string Name();
+	void Activate();
+	void Deactivate();
+};
+
+
+class ComponentRegistry {
+private:
+	std::vector<std::string> _registry;
+	unsigned int _registryCount = 0;
+public:
+	template <class C> void Register();
+	template <class C> unsigned int GetIndex();
+};
+
+template <class C>
+void ComponentRegistry::Register() {
+	if(!std::is_base_of<Component, C>::value) {
+		throw std::invalid_argument("Class is not a component");
+	}
+
+	unsigned int preRC = _registryCount;
+	C object;
+
+	if(std::find(_registry.begin(), _registry.end(), object.Name()) != _registry.end()) {
+		throw std::logic_error("A Component has already been registered under the " + object._name + " identifier");
+	}
+	_registry.push_back(object._name);
+	if(_registry.size() <= preRC) {
+		throw std::runtime_error("Component registration failed");
+	}
+}
+
+template <class C> 
+unsigned int ComponentRegistry::GetIndex() {
+	if(!std::is_base_of<Component, C>::value) {
+		throw std::invalid_argument("Class is not a component");
+	}
+
+	C object;
+	auto iterator = std::find(_registry.begin(), _registry.end(), object.Name);
+	if(iterator == _registry.end()) {
+		throw std::invalid_argument("Could not find component");
+	}
+	else {
+		return std::distance(_registry.begin(), iterator);
+	}
+}
---
+++ photon/src/component.cpp
@@ -0,0 +1,19 @@
+#include "component.hpp"
+
+using std::string;
+
+Component::Component(string name) {
+	_name = name;
+}
+
+string Component::Name() {
+	return _name;
+}
+
+void Component::Activate() {
+	_isActive = true;
+}
+
+void Component::Deactivate() {
+	_isActive = false;
+}
