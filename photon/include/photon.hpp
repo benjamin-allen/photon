1abb6361cd99c0347d8a185599e0f555f26d2e7a
Nullsrc <beallen529@gmail.com>
2017-08-04 02:00:24 -0500

    Add an easier header

---
+++ photon/include/photon.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "component.hpp"
+#include "entitymanager.hpp"
+#include "system.hpp"
+
+#define PHOTON_VERSION_MAJOR 0
+#define PHOTON_VERSION_MINOR 1
+#define PHOTON_VERSION_PATCH 0
